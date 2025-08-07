2fe97a26603acfb06f682aea81566f07b505b7b0
YashAgarwal1201 <legoyashx@gmail.com>
2025-08-07 22:54:17 +0530

    chore: placeholders for widgets

---
+++ src/components/Widgets/BasicProfile.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Basic Profile</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const feature = ref();
+</script>
+
+<style lang="css" scoped></style>
---
+++ src/components/Widgets/NotificationsAlerts.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Notifications</div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const feature = ref();
+</script>
+
+<style lang="css" scoped></style>
