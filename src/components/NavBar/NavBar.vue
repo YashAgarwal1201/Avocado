78cdbe0d9c9a7f670b47822d9387f4ca1e51d26a
YashAgarwal1201 <legoyashx@gmail.com>
2025-08-07 22:47:36 +0530

    chore: vite-ts setup with primevue and tailwind

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="w-full h-full flex flex-row md:flex-col items-center gap-1 text-white p-1"
+  >
+    <Button
+      class="w-auto md:w-full h-full md:h-auto aspect-square flex justify-center items-center !rounded-2xl"
+      ><Home :size="16" />
+    </Button>
+
+    <Button
+      class="w-auto md:w-full h-full md:h-auto aspect-square flex justify-center items-center !rounded-2xl"
+      ><MessageCircle :size="16" />
+    </Button>
+
+    <Button
+      class="w-auto md:w-full h-full md:h-auto aspect-square flex justify-center items-center !rounded-2xl ml-auto md:ml-0 mt-0 md:mt-auto"
+      ><Menu :size="16" />
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Home, Menu, MessageCircle } from "lucide-vue-next";
+import { Button } from "primevue";
+
+// import { ref } from 'vue';
+</script>
+
+<style lang="css" scoped>
+.aspect-square {
+  aspect-ratio: 1/1;
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,10 @@
+<template>
+  <BaseLayout><div>Home</div></BaseLayout>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import BaseLayout from "../layout/BaseLayout.vue";
+
+const feature = ref();
+</script>
