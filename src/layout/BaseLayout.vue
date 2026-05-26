22cea92f03d631e6f37b41e011af2ea31604f5ab
YashAgarwal1201 <legoyashx@gmail.com>
2026-05-26 17:35:37 +0530

    feat: new pill-shaped navbar design

--- src/layout/BaseLayout.vue
+++ src/layout/BaseLayout.vue
@@ -1,32 +1,30 @@
 <template>
-  <div class="w-full h-full flex flex-col md:flex-row">
-    <Toast class="!max-w-2xs"/>
-    <div class="w-full md:w-[70px] h-[60px] md:h-full flex-shrink-0">
-      <NavBar/>
+  <div class="w-full h-full flex flex-col-reverse md:flex-row">
+    <Toast class="max-w-2xs!" />
+    <nav class="w-full md:w-[70px] h-[60px] md:h-full shrink-0">
+      <NavBar />
+    </nav>
+    <div class="h-full grow overflow-auto">
+      <slot />
     </div>
-    <div class="h-full flex-grow-1 overflow-auto">
-      <slot/>
-    </div>
-
-
   </div>
-  <SideMenu/>
-  <FeedbackMenu/>
+  <SideMenu />
+  <FeedbackMenu />
   <ConfirmDialog></ConfirmDialog>
 </template>
 
 <script lang="ts" setup>
-import {Toast} from "primevue"
-import ConfirmDialog from "primevue/confirmdialog"
+import { Toast } from "primevue";
+import ConfirmDialog from "primevue/confirmdialog";
 import NavBar from "../components/NavBar/NavBar.vue";
 import SideMenu from "../components/NavBar/SideMenu.vue";
 import FeedbackMenu from "../components/NavBar/FeedbackMenu.vue";
-import {onMounted} from "vue";
-import {useGithubStore} from "../pinia/githubProfileStore.ts";
+import { onMounted } from "vue";
+import { useGithubStore } from "../pinia/githubProfileStore.ts";
 import toastHandler from "../composables/toastHandler.ts";
 
 const githubStore = useGithubStore();
-const {showToast} = toastHandler()
+const { showToast } = toastHandler();
 
 onMounted(() => {
   if (githubStore.token) {
@@ -36,7 +34,7 @@ onMounted(() => {
       githubStore.fetchGithubData();
     }
   } else {
-    showToast("error", "Error", "No github token found.")
+    showToast("error", "Error", "No github token found.");
   }
 });
 </script>
